fc3a275e37ba29e98cc42693eac3ca43d0c3b25f
Pierre Bourdais <pierre.bourdais@kody-gp.fr>
2024-05-28 19:29:13 +0200

    Update

---
+++ lib/Screens/Home.dart
@@ -0,0 +1,39 @@
+// import 'package:flutter/material.dart';
+//
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key? key}) : super(key: key);
+//
+//   @override
+//   _MyHomePage createState() => _MyHomePage();
+// }
+//
+// class _MyHomePage extends State<MyHomePage> {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//           appBar: AppBar(
+//             title: Text("JE SUIS UN TITRE"),
+//           ),
+//           body: Container(
+//               child: Center(
+//                 child: Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: <Widget>[
+//                     Text("Hello World"),
+//                     ElevatedButton(
+//                       onPressed: () {
+//                         print("Hello World");
+//                       },
+//                       child: Text("Click me"),
+//                     )
+//                   ],
+//                 ),
+//               )
+//           )
+//       ),
+//     );
+//   }
+// }
+//
